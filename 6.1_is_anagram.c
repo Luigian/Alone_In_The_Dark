cbec9e7426341bd8dda0d2667db0b9cae91d7aa2
Luis Sanchez <loic.quiros@gmail.com>
2019-11-26 11:44:33 -0800

    queue

--- 6.1_is_anagram.c
+++ 6.1_is_anagram.c
@@ -5,42 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/19 09:04:04 by exam              #+#    #+#             */
-/*   Updated: 2019/11/26 08:16:45 by lusanche         ###   ########.fr       */
+/*   Created: 2019/11/26 09:02:31 by exam              #+#    #+#             */
+/*   Updated: 2019/11/26 09:14:10 by exam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include <stdio.h>
-
+*/
 int		is_anagram(char *a, char *b)
 {
-	int		arr[127];
+	int		array[128] = {0};
 	int		i;
 
-	i = 0;
-	while (i < 127)
-		arr[i++] = 0;
 	while (*a)
 	{
-		arr[(int)*a] += 1;
+		array[(int)*a] += 1;
 		++a;
 	}
 	while (*b)
 	{
-		arr[(int)*b] -= 1;
+		array[(int)*b] -= 1;
 		++b;
 	}
-	i = 32;
-	while (i < 127)
+	i = 0;
+	while (i < 128)
 	{
-		if (arr[i++])
+		if (array[i++])
 			return (0);
 	}
 	return (1);
 }
-
-int	main(void)
+/*
+int		main(void)
 {
 	printf("%d\n", is_anagram("cinema", "iceman"));
 	return (0);
 }
+*/
--- 7.1_queue.c
+++ 7.1_queue.c
@@ -5,20 +5,15 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/11/19 10:06:59 by exam              #+#    #+#             */
-/*   Updated: 2019/11/19 11:32:52 by exam             ###   ########.fr       */
+/*   Created: 2019/11/26 09:18:21 by exam              #+#    #+#             */
+/*   Updated: 2019/11/26 10:47:10 by exam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// WARNING : THIS EXERCISE HAVE SOME ERRORS
-
-
 #include <stdlib.h>
-
 /*
 #include <stdio.h>
 */
-
 struct s_node {
 	void *content;
 	struct s_node *next;
@@ -29,97 +24,112 @@ struct s_queue {
 	struct s_node *last;
 };
 
-struct s_queue *init(void)
+struct s_queue	*init(void)
 {
-	struct s_queue		*cola;
+	struct s_queue	*q;
 
-	cola = (struct s_queue *)malloc(sizeof(struct s_queue) * 1);
-	cola->first = NULL;
-	cola->last = NULL;
-	return (cola);
+	q = (struct s_queue*)malloc(sizeof(struct s_queue) * 1);
+	q->first = NULL;
+	q->last = NULL;
+	return (q);
 }
 
-/*
-struct s_node *init_item(void)
+struct s_node	*init_node(void *content)
 {
-	struct s_node		*item;
+	struct s_node	*n;
 
-	item = (struct s_node *)malloc(sizeof(struct s_node) * 1);
-	item->content = "holaaa";
-	item->next = NULL;
-	return (item);
+	n = (struct s_node*)malloc(sizeof(struct s_node) * 1);
+	n->content = content;
+	n->next = NULL;
+	return (n);
 }
-*/
 
 void	enqueue(struct s_queue *queue, void *content)
 {
+	struct s_node	*n;
+
+	n = init_node(content);	
 	if (queue->last == NULL)
 	{
-		queue->first = content;
-		queue->last = content;
+		queue->first = n;
+		queue->last = n;
 	}
 	else
 	{
-		queue->last->next = content;
-		queue->last = content;
+		queue->last->next = n;
+		queue->last = n;
 	}
 }
 
 void	*dequeue(struct s_queue *queue)
 {
-	struct s_node		*temp;
 	
+	void			*ret;
+
 	if (queue->first == NULL)
 		return (NULL);
-	else
+	ret = queue->first->content;
+	if (queue->first->next == NULL)
 	{
-		temp = queue->first;
-		if (queue->first->next == NULL)
-			queue->last = NULL;
-		queue->first = queue->first->next;
-		temp->next = NULL;
+		queue->first = NULL;
+		queue->last = NULL;
 	}
-	return (temp);
+	else
+		queue->first = queue->first->next;
+	return (ret);
 }
-
+	 
 void	*peek(struct s_queue *queue)
 {
-	struct s_node		*temp;
-	
+	void			*ret;
+
 	if (queue->first == NULL)
 		return (NULL);
-	else
-		temp = queue->first;
-	return (temp);
+	ret = queue->first->content;
+	return (ret);
 }
 
-int		isEmpty(struct s_queue *queue)
+int isEmpty(struct s_queue *queue)
 {
 	if (queue->first == NULL)
 		return (1);
 	return (0);
 }
-
 /*
 int		main(void)
 {
-	struct s_queue		*cola;
-	struct s_node		*item;
-	struct s_node		*new;
-	struct s_node		*beg;
+	struct s_queue	*q;
+	struct s_node	*n;
 
-	cola = init();
-	printf("%p\n", (void *)cola->last);
-	item = init_item();
-	enqueue(cola, item);
-	printf("%p\n", (void *)cola->last);
-	printf("%s\n", cola->last->content);
-	new = dequeue(cola);
-	printf("%p\n", (void *)cola->last);
-	printf("%p\n", (void *)new);
-	printf("%s\n", new->content);
-	beg = peek(cola);
-	printf("%p\n", (void *)beg);
-	printf("%d\n", isEmpty(cola));
+	q = init();
+	printf("0x0 0x0:\t%p\t%p\n", q->first, q->last);
+	n = init_node("Juan");
+	printf("Juan:\t\t%s\n", n->content);
+	enqueue(q, "Pedro");
+	printf("Pedro\tPedro:\t%s\t%s\n", q->last->content, q->first->content);
+	printf("& &:\t\t%p\t%p\n", q->first, q->last);
+	printf("Pedro:\t\t%s\n", dequeue(q));
+	printf("0x0 0x0:\t%p\t%p\n", q->first, q->last);
+	enqueue(q, "Oscar");
+	printf("Oscar:\t\t%s\n", peek(q));
+	printf("0:\t\t%d\n", isEmpty(q));	
+	printf("Oscar:\t\t%s\n", dequeue(q));
+	printf("1:\t\t%d\n", isEmpty(q));	
+	enqueue(q, "one");
+	enqueue(q, "two");
+	enqueue(q, "three");
+	enqueue(q, "four");
+	enqueue(q, "five");
+	printf("one two three four five:\t%s %s %s %s %s \n", q->first->content,\
+		 q->first->next->content, q->first->next->next->content,\
+		 q->first->next->next->next->content, q->last->content);
+	printf("one:\t\t%s\n", peek(q));
+	printf("one:\t\t%s\n", dequeue(q));
+	printf("two:\t\t%s\n", dequeue(q));
+	printf("three:\t\t%s\n", dequeue(q));
+	printf("four:\t\t%s\n", dequeue(q));
+	printf("five:\t\t%s\n", dequeue(q));
+	printf("1:\t\t%d\n", isEmpty(q));	
 	return (0);
-}*/
+}
+*/
