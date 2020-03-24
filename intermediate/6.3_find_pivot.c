d1e10b30ab5e1fa812cfdaca386ed0242d251ea9
Luis <loic.quiros@gmail.com>
2020-03-24 14:02:06 -0700

    find_pivot optimized

--- intermediate/6.3_find_pivot.c
+++ intermediate/6.3_find_pivot.c
@@ -1,64 +1,39 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   find_pivot.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/02/04 09:02:41 by exam              #+#    #+#             */
-/*   Updated: 2020/02/04 09:20:15 by exam             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/*
 #include <stdio.h>
-*/
 
-int		pv_total(int *arr, int n)
+int	find_pivot(int	*arr, int n)
 {
-	int		total;
-	int		i;
+	int	i;
+	int	total;
+	int	left;
 
+	i = 0;
 	total = 0;
-	i = 0;
-	while (i < n)
-	{
-		total = total + arr[i];
-		++i;
-	}
-	return (total);
-}
-
-int		find_pivot(int *arr, int n)
-{
-	int		total;
-	int		i;
-	int		left;
-
-	total = pv_total(arr, n);
-	i = 0;
 	left = 0;
 	while (i < n)
+		total += arr[i++];
+	i = 0;
+	while (i < n)
 	{
-		left = left + arr[i];
+		left += arr[i];
 		if ((total - left) == (left - arr[i]))
-			return (i);
+				return (i);
 		++i;
 	}
 	return (-1);
 }
 
-/*
-int		main(void)
+int	main(void)
 {
-	int		arr[] = {1, 2, 3, 4, 0, 6};
-	int		arr[] = {-5, 10, 2, 5};
-	int		arr[] = {1, 100, 0, 0, 1};
-	int		arr[] = {7, 9, 8};
-	int		arr[] = {1, 2};
-	int		arr[] = {0, 0};
+	int	arr1[] = {1, 2, 3, 4, 0, 6};
+	int	arr2[] = {-5, 10, 2, 5};
+	int	arr3[] = {1, 100, 0, 0, 1};
+	int	arr4[] = {7, 9, 8};
+	int	arr5[] = {1, 2};
 
-	printf("%d\n", find_pivot(arr, 2));
+	printf("%d\n", find_pivot(arr1, 6));
+	printf("%d\n", find_pivot(arr2, 4));
+	printf("%d\n", find_pivot(arr3, 5));
+	printf("%d\n", find_pivot(arr4, 3));
+	printf("%d\n", find_pivot(arr5, 2));
 	return (0);
 }
-*/
