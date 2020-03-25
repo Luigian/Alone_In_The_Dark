b8760532f874615db5bd2bccfe5f39a8c9cc3f89
Luis <loic.quiros@gmail.com>
2020-03-25 14:13:54 -0700

    print_doublon optimized

--- intermediate/6.4_print_doublon.c
+++ intermediate/6.4_print_doublon.c
@@ -1,30 +1,16 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   print_doublon.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/10 09:03:09 by exam              #+#    #+#             */
-/*   Updated: 2020/03/10 09:33:26 by exam             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 
 void	print_doublon(int *a, int na, int *b, int nb)
 {
-	int		i;
-	int		j;
-	int		x;
-	
-	i = 0;
-	j = 0;
-	x = 0;
+	int	i = 0;
+	int	j = 0;
+	int	x = 0;
+		
 	while (i < na && j < nb)
 	{
 		if (a[i] == b[j])
 		{
+			
 			if (x)
 				printf(" ");
 			printf("%d", a[i]);
@@ -34,29 +20,23 @@ void	print_doublon(int *a, int na, int *b, int nb)
 		}
 		else if (a[i] < b[j])
 			++i;
-		else if (b[j] < a[i])
+		else
 			++j;
 	}
 	printf("\n");
 }
 
-/*
-int		main(void)
+int	main(void)
 {
-//	int		a[] = {1, 2, 10, 15};
-//	int		b[] = {2, 20, 40, 70};
-//	int		na = 4;
-//	int		nb = 4;
-//	int		a[] = {-5, 2, 10, 15, 50, 70, 100, 200, 300, 1200, 5000};
-//	int		b[] = {2, 4, 5, 6, 7, 10, 40, 70};
-//	int		na = 11;
-//	int		nb = 8;
-	int		a[] = {100, 200, 300};
-	int		b[] = {1, 2, 3, 4};
-	int		na = 3;
-	int		nb = 4;
-
-	print_doublon(a, na, b, nb);
+	int	arr1[] = {1, 2, 10, 15};
+	int	arr2[] = {2, 20, 40, 70};
+	int	arr3[] = {-5, 2, 10, 15, 50, 70, 100, 200, 300, 1200, 5000};
+	int	arr4[] = {2, 4, 5, 6, 7, 10, 40, 70};
+	int	arr5[] = {100, 200, 300};
+	int	arr6[] = {1, 2, 3, 4};
+	
+	print_doublon(arr1, 4, arr2, 4);
+	print_doublon(arr3, 11, arr4, 8);
+	print_doublon(arr5, 3, arr6, 4);
 	return (0);
 }
-*/
