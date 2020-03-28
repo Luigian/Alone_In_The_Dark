613a8a360101d314ea4f57f6a978f5729f424528
Luis <loic.quiros@gmail.com>
2020-03-28 16:23:35 -0700

    stack optimized

--- intermediate/7.4_stack.c
+++ intermediate/7.4_stack.c
@@ -1,29 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   stack.c                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/14 10:44:24 by exam              #+#    #+#             */
-/*   Updated: 2020/01/14 11:39:23 by exam             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
-
-/*
 #include <stdio.h>
-*/
 
-struct s_node
-{
-	void			*content;
-	struct s_node 	*next;
+struct s_node {
+	void		*content;
+	struct s_node	*next;
 };
 
-struct s_stack
-{
+struct s_stack {
 	struct s_node	*top;
 };
 
@@ -36,69 +19,55 @@ struct s_stack	*init(void)
 	return (s);
 }
 
-struct s_node	*init_node(void *data)
+void	push(struct s_stack *stack, void *content)
 {
 	struct s_node	*n;
 
 	n = malloc(sizeof(struct s_node));
-	n->content = data;
-	n->next = NULL;
-	return (n);
+	n->content = content;
+	n->next = stack->top;
+	stack->top = n;
+}
+
+void	*peek(struct s_stack *stack)
+{
+	if (stack->top != NULL)
+		return (stack->top->content);
+	return (NULL);
 }
 
 void	*pop(struct s_stack *stack)
 {
 	void	*ret;
 
-	if (stack->top == NULL)
-		return (NULL);
-	ret = stack->top->content;
-	stack->top = stack->top->next;
-	return (ret);
-}
-
-void	push(struct s_stack *stack, void *content)
-{
-	struct s_node	*n;
-	
-	n = init_node(content);
-	if (stack->top == NULL)
-		stack->top = n;
-	else
+	ret = NULL;
+	if (stack->top != NULL)
 	{
-		n->next = stack->top;
-		stack->top = n;
+		ret = stack->top->content;
+		stack->top = stack->top->next;
 	}
-}
+	return (ret);
+}			
 
-void	*peek(struct s_stack *stack)
+int	isEmpty(struct s_stack *stack)
 {
-	if (stack->top == NULL)
-		return (NULL);
-	return (stack->top->content);
+	if (stack->top != NULL)
+		return (0);
+	return (1);
 }
 
-int		isEmpty(struct s_stack *stack)
-{
-	if (stack->top == NULL)
-		return (1);
-	return (0);
-}
-
-/*
-int		main(void)
+int	main(void)
 {
 	struct s_stack	*s;
 
 	s = init();
-	printf("no address: %p\n", s->top);
-	push(s, "Peter");
-	printf("yes address: %p\n", s->top);
-	printf("peek: %s\n", peek(s));
-	printf("yes address: %p\n", s->top);
-	printf("pop: %s\n", pop(s));
-	printf("no address: %p\n", s->top);
-	printf("one: %d\n", isEmpty(s));
+	printf("%p\n", s->top);
+	push(s, "monday");
+	printf("%p\n", s->top);
+	printf("%s\n", (char *)s->top->content);
+	printf("%s\n", (char *)peek(s));
+	printf("%s\n", (char *)pop(s));
+	printf("%d\n", isEmpty(s));
+	printf("%p\n", s->top);
 	return (0);
 }
-*/
