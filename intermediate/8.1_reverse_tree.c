0342c9319b1ee8ef0c20b07fe7a9fd78e3e2d435
Luis <loic.quiros@gmail.com>
2020-04-01 13:50:50 -0700

    reverse_tree update

--- intermediate/8.1_reverse_tree.c
+++ intermediate/8.1_reverse_tree.c
@@ -1,86 +1,86 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   reverse_tree.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/01/23 11:21:18 by exam              #+#    #+#             */
-/*   Updated: 2020/01/23 11:45:03 by exam             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/*
+#include <stdlib.h>
 #include <stdio.h>
-*/
 
-struct s_node
-{
-	int				value;
+struct s_node {
+	int		value;
 	struct s_node	*right;
-	struct s_node 	*left;
+	struct s_node	*left;
 };
+
+void	print_tree(struct s_node *n)
+{
+	printf("%d\n", n->value);
+	if (n->left)
+		print_tree(n->left);
+	if (n->right)
+		print_tree(n->right);
+}
 
 void	reverse_tree(struct s_node *root)
 {
-	struct	s_node	*temp;
+	struct s_node	*temp;
 
 	temp = root->left;
 	root->left = root->right;
 	root->right = temp;
-
 	if (root->left)
 		reverse_tree(root->left);
 	if (root->right)
 		reverse_tree(root->right);
 }
 
-/*
-void	print_tree(struct s_node *root)
+int	main(void)
 {
-	printf("%d\n", root->value);
-	if (root->left)
-		print_tree(root->left);
-	if (root->right)
-		print_tree(root->right);
-}
-
-int		main(void)
-{
-	struct	s_node	root;
-	struct	s_node	a;
-	struct	s_node	b;
-	struct	s_node	c;
-	struct	s_node	d;
-	struct	s_node	e;
-	struct	s_node	f;
-
-	root.value = 1;
-	a.value = 2;
-	b.value = 3;
-	c.value = 4;
-	d.value = 5;
-	e.value = 6;
-	f.value = 7;
+	struct s_node	n1;
+	struct s_node	n2;
+	struct s_node	n3;
+	struct s_node	n4;
+	struct s_node	n5;
+	struct s_node	n6;
+	struct s_node	n7;
+	struct s_node	n8;
+	struct s_node	n9;
+	struct s_node	n10;
+	struct s_node	n11;
 	
-	root.left = &a;
-	root.right = &b;
-	a.left = &c;
-	a.right = &d;
-	b.left = &e;
-	b.right = &f;
-	c.left = NULL;
-	c.right = NULL;
-	d.left = NULL;
-	d.right = NULL;
-	e.left = NULL;
-	e.right = NULL;
-	f.left = NULL;
-	f.right = NULL;
+	n1.value = 94;
+	n2.value = 34;
+	n3.value = 52;
+	n4.value = 1;
+	n5.value = 99;
+	n6.value = 20;
+	n7.value = 83;
+	n8.value = 39;
+	n9.value = 61;
+	n10.value = 37;
+	n11.value = 67;
 
-	print_tree(&root);
-	reverse_tree(&root);
-	print_tree(&root);
+	n1.left = &n2;
+	n1.right = &n3;
+	n2.left = &n4;
+	n2.right = &n5;
+	n3.left = NULL;
+	n3.right = NULL;
+	n4.left = &n6;
+	n4.right = NULL;
+	n5.left = &n7;
+	n5.right = &n8;
+	n6.left = NULL;
+	n6.right = NULL;
+	n7.left = NULL;
+	n7.right = &n9;
+	n8.left = &n10;
+	n8.right = &n11;
+	n9.left = NULL;
+	n9.right = NULL;
+	n10.left = NULL;
+	n10.right = NULL;
+	n11.left = NULL;
+	n11.right = NULL;
+	
+	print_tree(&n1);
+	printf("\n");	
+	reverse_tree(&n1);
+	print_tree(&n1);	
 	return (0);
 }
-*/
