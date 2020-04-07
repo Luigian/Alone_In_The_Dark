cf0ad8db73f9185c9c5d832b4b5a0df49cb2d214
Luis <loic.quiros@gmail.com>
2020-04-07 13:29:04 -0700

    longest_sequence optimized

--- intermediate/8.4_longest_sequence.c
+++ intermediate/8.4_longest_sequence.c
@@ -1,115 +1,88 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   longest_sequence.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/03 10:02:07 by exam              #+#    #+#             */
-/*   Updated: 2020/03/03 10:54:09 by exam             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-/*
-#include <stdio.h>
 #include <stdlib.h>
-*/
+#include <stdio.h>
 
-struct s_node
-{
-	int value;
-	struct s_node *left;
-	struct s_node *right;
+struct s_node {
+	int		value;
+	struct s_node	*left;
+	struct s_node	*right;
 };
+
+void	print_tree(struct s_node *root)
+{
+	printf("%d\n", root->value);
+	if (root->left)
+		print_tree(root->left);
+	if (root->right)
+		print_tree(root->right);
+}
 
 void	ls_recursion(struct s_node *node, int len, int *max)
 {
-	if (len > *max)
-		*max = len;
 	if (node->left)
 	{
-		if ((node->value + 1) == node->left->value)
+		if (node->value + 1 == node->left->value)
 			ls_recursion(node->left, len + 1, max);
 		else
 			ls_recursion(node->left, 1, max);
 	}
 	if (node->right)
 	{
-		if ((node->value + 1) == node->right->value)
+		if (node->value + 1 == node->right->value)
 			ls_recursion(node->right, len + 1, max);
 		else
 			ls_recursion(node->right, 1, max);
 	}
+	if (len > *max)
+		*max = len;
 }
 
-int		longest_sequence(struct s_node *node)
+int	longest_sequence(struct s_node *node)
 {
-	int		len;
-	int		max;
+	int	len;
+	int	max;
 
 	len = 1;
-	max = len;
+	max = 1;
 	if (!node)
 		return (0);
 	ls_recursion(node, len, &max);
 	return (max);
 }
 
-/*
-void	print_tree(struct s_node *node)
+int	main(void)
 {
-	printf("%d\n", node->value);
-	if (node->left)
-		print_tree(node->left);
-	if (node->right)
-		print_tree(node->right);
-}
-*/
-/*
-int		main(void)
-{
-	struct s_node	*x;
-	x = NULL;
-		
-	struct s_node	a;
-	struct s_node	b;
-	struct s_node	c;
-	struct s_node	d;
-	struct s_node	e;
-	struct s_node	f;
-	struct s_node	g;
-	struct s_node	h;
+	struct s_node	n1;
+	struct s_node	n2;
+	struct s_node	n3;
+	struct s_node	n4;
+	struct s_node	n5;
+	struct s_node	n6;
+	struct s_node	*n7 = NULL;
 
-	a.value = 5;
-	b.value = 6;
-	c.value = 4;
-	d.value = 7;
-	e.value = 3;
-	f.value = 3;
-	g.value = 2;
-	h.value = 2;
+	n1.value = 10;
+	n2.value = 5;
+	n3.value = 6;
+	n4.value = 9;
+	n5.value = 7;
+	n6.value = 13;
 
-	a.left = &b;
-	a.right = &c;
-	b.left = NULL;
-	b.right = NULL;
-	c.left = &d;
-	c.right = &e;
-	d.left = &f;
-	d.right = &g;
-	e.left = NULL;
-	e.right = &h;
-	f.left = NULL;
-	f.right = NULL;
-	g.left = NULL;
-	g.right = NULL;
-	h.left = NULL;
-	h.right = NULL;
+	n1.left = &n2;
+	n1.right = NULL;
+	n2.left = &n3;
+	n2.right = &n4;
+	n3.left = &n5;
+	n3.right = &n6;
+	n4.left = NULL;
+	n4.right = NULL;
+	n5.left = NULL;
+	n5.right = NULL;
+	n6.left = NULL;
+	n6.right = NULL;
 
-//	print_tree(&a);
-	printf("%d\n", longest_sequence(&a));
-	printf("%d\n", longest_sequence(&c));
-	printf("%d\n", longest_sequence(x));
+//	print_tree(&n1);
+	printf("%d\n", longest_sequence(&n1));
+	printf("%d\n", longest_sequence(&n3));
+	printf("%d\n", longest_sequence(&n6));
+	printf("%d\n", longest_sequence(n7));
 	return (0);
 }
-*/
