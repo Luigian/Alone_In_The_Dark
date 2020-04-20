9dc6ccd3f96e1f110b0288d09151e4d1b09c69a1
Luis <loic.quiros@gmail.com>
2020-04-20 15:34:50 -0700

    is_anagram optimized

--- intermediate/6.1_is_anagram.c
+++ intermediate/6.1_is_anagram.c
@@ -1,56 +1,37 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   is_anagram.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: exam <marvin@42.fr>                        +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/17 09:02:12 by exam              #+#    #+#             */
-/*   Updated: 2019/12/17 13:04:05 by lusanche         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
-/*
-void	print_a(int a[])
-{
-	int i;
 
-	i	= 0;
+int	is_anagram(char *a, char *b)
+{
+	int	arr[128];
+	int	i;
+
+	i = 0;
 	while (i < 128)
-		printf("%d", a[i++]);
-	printf("\n");
-}
-*/
-int		is_anagram(char *a, char *b)
-{
-	int		arr[128] = {0};
-	int		i;
-
-//	print_a(arr);
-	while (*a)
+		arr[i++] = 0;
+	i = 0;
+	while (a[i])
 	{
-		arr[(int)*a] += 1;
-		++a;
+		arr[(int)a[i]] += 1;
+		++i;
 	}
-//	print_a(arr);
-	while (*b)
+	i = 0;
+	while (b[i])
 	{
-		arr[(int)*b] -= 1;
-		++b;
+		arr[(int)b[i]] -= 1;
+		++i;
 	}
-//	print_a(arr);
 	i = 0;
 	while (i < 128)
 	{
-		if (arr[i++])
+		if (arr[i])
 			return (0);
+		i++;
 	}
 	return (1);
 }
 
-int		main(void)
+int	main(void)
 {
 	printf("%d\n", is_anagram("cinema", "iceman"));
 	return (0);
-}
+}	
